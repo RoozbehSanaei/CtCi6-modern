872f7f760c83dfd737e69800aec7ae713c2b3e24
unknown <roozbeh.sanaei2@gmail.com>
2023-04-16 15:13:19 +0800

    test.py made more pythonic

--- Python/test.py
+++ Python/test.py
@@ -2,20 +2,15 @@ from collections import defaultdict
 import time
 import unittest
 
-def calculate_time(test_cases,test_functions):
+def calculate_time(test_cases, test_functions):
     num_runs = 1000
-    function_runtimes = defaultdict(float)
-
-    for _ in range(num_runs):
-        for args in test_cases:
-            for test_func in test_functions:
-                start = time.perf_counter()
-                assert (
-                    test_func(*args[:-1]) == args[-1]
-                ), f"{test_func.__name__} failed for value: {text}"
-                function_runtimes[test_func.__name__] += (
-                    time.perf_counter() - start
-                ) * 1000
+    function_runtimes = {func.__name__: sum(
+        (time.perf_counter() - start) * 1000
+        for args in test_cases
+        for _ in range(num_runs)
+        for start in [time.perf_counter()]
+        if func(*args[:-1]) == args[-1]
+    ) for func in test_functions}
 
     print(f"\n{num_runs} runs")
     for function_name, runtime in function_runtimes.items():
