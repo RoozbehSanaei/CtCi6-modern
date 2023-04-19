a3625f27a72ad5b5b90c981cd61d500f52e6999c
Roozbeh.Sanaei2@gmail.com <roozbeh.sanaei2@gmail.com>
2023-04-19 11:59:24 +0800

    check permutation is transformed to C++11

--- C++/check_permutation.cpp
+++ C++/check_permutation.cpp
@@ -4,82 +4,74 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
-#include <ranges>
 #include <cassert>
 
-
 using namespace std;
-bool arePermutation(string str1,string str2)
+
+
+bool arePermutation(const std::string& str1, const std::string& str2)
 {
-    // Get lengths of both strings
-    int n1 = str1.length();
-    int n2 = str2.length();
+  if (str1.size() != str2.size()) return false;
 
-    // If length of both strings is not same, then they
-    // cannot be anagram
-    if (n1 != n2)
-      return false;
+  std::string sorted_str1 = str1;
+  std::sort(sorted_str1.begin(), sorted_str1.end());
+  std::string sorted_str2 = str2;
+  std::sort(sorted_str2.begin(), sorted_str2.end());
 
-    // Sort both strings
-    sort(str1.begin(), str1.end());
-    sort(str2.begin(), str2.end());
-    // Compare sorted strings
-    for (int i = 0; i < n1;  i++)
-       if (str1[i] != str2[i])
-         return false;
-
-    return true;
+  return sorted_str1 == sorted_str2;
 }
 
-bool arePermutation_2(const string &str1, const string &str2) {
-  if(str1.length() != str2.length()) 
+bool arePermutation_2(const std::string &str1, const std::string &str2)
+{
+  if (str1.length() != str2.length())
+  {
     return false;
-  int count[256]={0};
-  for(int i = 0; i < str1.length(); i++) {
-    int val = str1[i];
-    count[val]++;
   }
-  for(int i = 0; i < str2.length(); i++) {
-    int val = str2[i];
-    count[val]--;
-    if(count[val]<0) 
+
+  std::array<int, 256> count{};
+  for (char c : str1)
+  {
+    count[static_cast<unsigned char>(c)]++;
+  }
+  for (char c : str2)
+  {
+    count[static_cast<unsigned char>(c)]--;
+    if (count[static_cast<unsigned char>(c)] < 0)
+    {
       return false;
+    }
   }
+
   return true;
 }
 
-int multiply(int a, int b) {
-    return a * b;
-}
+int main()
+{
 
-int main() {
+  std::vector<std::tuple<string, string, bool>> test_cases = {
+      {"testest", "estxest", false},
+      {"hello", "oellh", true},
+  };
 
-        std::vector<std::tuple<string, string, bool>> test_cases = {
-        {"testest", "estxest", false},
-        {"hello", "oellh", true},
-    };
+  // perform tests using assert and range-based for loop
+  int i = 1;
+  for (auto [a, b, expected_output] : test_cases)
+  {
+    int output = arePermutation(a, b);
+    assert(output == expected_output);
+    std::cout << "Method 1, Test case " << i++ << " passed\n";
+  }
 
+  // perform tests using assert and range-based for loop
+  i = 1;
+  for (auto [a, b, expected_output] : test_cases)
+  {
+    int output = arePermutation_2(a, b);
+    assert(output == expected_output);
+    std::cout << "Method 2, Test case " << i++ << " passed\n";
+  }
 
+  std::cout << "All tests passed\n";
 
-    // perform tests using assert and range-based for loop
-    int i = 1;
-    for (auto [a, b, expected_output] : test_cases) {
-        int output = arePermutation(a, b);
-        assert(output == expected_output);
-        std::cout << "Method 1, Test case " << i++ << " passed\n";
-    }
-
-        // perform tests using assert and range-based for loop
-    i = 1;
-    for (auto [a, b, expected_output] : test_cases) {
-        int output = arePermutation_2(a, b);
-        assert(output == expected_output);
-        std::cout << "Method 2, Test case " << i++ << " passed\n";
-    }
-
-    std::cout << "All tests passed\n";
-
-    return 0;
-
-
+  return 0;
 }
