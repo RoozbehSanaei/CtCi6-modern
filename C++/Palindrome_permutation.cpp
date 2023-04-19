c0261b66a2d3dc9e80fc51202243838d0e42a134
unknown <roozbeh.sanaei2@gmail.com>
2023-04-19 18:40:38 +0800

    Palndrom permutation comments added

--- C++/Palindrome_permutation.cpp
+++ C++/Palindrome_permutation.cpp
@@ -52,6 +52,20 @@ std::vector<int> buildCharFrequencyTable(const std::string& phrase){
     return table;
 }
 
+/*
+The function takes in a constant reference to a vector of integers called "table". 
+The function uses the "std::count_if" algorithm from the <algorithm> header file 
+to count the number of elements in the "table" vector that satisfy the condition given in the lambda function. 
+The lambda function takes an integer "count" and returns true if "count" is odd 
+(i.e., "count % 2 == 1").
+
+If the number of odd elements in "table" is less than or equal to 1,
+the function returns true, indicating that there is at most one odd element in the vector.
+If the number of odd elements is greater than 1, the function returns false, 
+indicating that there are more than one odd elements in the vector.
+
+
+*/
 
 bool checkMaxOneOdd(const std::vector<int>& table) {
     return std::count_if(table.begin(), table.end(), [](int count) {
@@ -65,6 +79,48 @@ bool isPermutationOfPalindrome(const string &phrase)
     return checkMaxOneOdd(table);
 }
 
+
+/*
+he function isPermutationOfPalindrome_bitwise takes a std::string_view argument phrase. 
+This is a lightweight object that provides a view of a sequence of characters in memory,
+ without actually owning or copying the characters. Since the function only needs to
+  read the characters in phrase, using a std::string_view instead of a std::string 
+  avoids unnecessary copying and memory allocation.
+
+A std::bitset is created with 26 bits, which is enough to represent all the lowercase 
+letters of the English alphabet. The std::bitset is initialized with all bits set to zero 
+by default.
+
+The for loop iterates over each character in phrase. 
+The loop variable c is declared as a const char, which means that its value 
+cannot be modified within the loop. This is a good practice to avoid unintentional side effects.
+
+The std::isalpha function is used to check if the character c is an alphabetic character.
+ This function returns true if the character is a letter of the alphabet (uppercase or lowercase), 
+ and false otherwise.
+
+If c is an alphabetic character, its case is normalized to lowercase using the std::tolower function.
+ This is done to treat uppercase and lowercase versions of the same letter as the same character.
+
+The ASCII value of the lowercase letter 'a' is subtracted from the normalized character, 
+which results in a zero-based index into the std::bitset. For example, the character 'a' 
+maps to index 0, 'b' maps to index 1, and so on. This mapping is possible because the ASCII
+ codes for the lowercase letters of the alphabet are contiguous and start at 97.
+
+The flip function is used to toggle the bit at the computed index in the std::bitset. 
+If the bit was previously set to 0, it becomes 1, and vice versa. This is an efficient 
+way to count the frequency of each letter in phrase.
+
+After the loop, the count function of the std::bitset is used to count the number of bits
+ set to 1. This corresponds to the number of letters in phrase that have an odd frequency. 
+ If this count is at most 1, phrase is a permutation of a palindrome, and the function returns 
+ true. Otherwise, the function returns false.
+
+The time complexity of the function is O(n), where n is the length of phrase. The space complexity
+ is O(1), since the std::bitset has a fixed size of 26 bits. 
+ The function is therefore very efficient in terms of time and space,
+  and can handle long input strings without causing memory or performance issues.
+*/
 
 bool isPermutationOfPalindrome_bitwise(std::string_view phrase) {
     std::bitset<26> table;
